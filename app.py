5e7e86a05d20e1b4235e1bf7195f1c0511e1e2f5
igorkpl <ik@letyshops.ru>
2017-07-13 17:19:30 +0300

    fix app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "LetyBot"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    debug = bool(os.environ.get('DEBUG', True))
+    app.run(host='0.0.0.0', port=port, debug=True)
