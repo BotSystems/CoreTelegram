b778dbc75dca415463054769ea85986869042ae4
igorkpl <ik@letyshops.ru>
2017-07-13 18:20:23 +0300

    testing

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from telegram.ext import Updater
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    return "LetyBot"
+    port = int(os.environ.get('PORT', 5000))
+    debug = bool(os.environ.get('DEBUG', True))
+    appname = os.environ.get('APPNAME', 'NO-APP-NAME')
+    token = os.environ.get('TOKEN', 'NO-TOKEN')
+    return "{}:{}:{}:{}".format(port, debug, appname, token)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = bool(os.environ.get('DEBUG', True))
-    print(port)
-    print(debug)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    appname = os.environ.get('APPNAME', 'NO-APP-NAME')
+    token = os.environ.get('TOKEN', 'NO-TOKEN')
+
+    updater = Updater(token)
+    updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+    updater.bot.set_webhook("https://{}.herokuapp.com/".format(appname) + token)
+    updater.idle()
+
+    app.run(host='0.0.0.0', port=port, debug=debug)
 
