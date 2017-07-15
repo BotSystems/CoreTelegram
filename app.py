7c862476b386b57a25c49effd0e80b6aa3b829f0
igorkpl <ik@letyshops.ru>
2017-07-15 20:35:59 +0300

    add schemas: longpooling, webhook

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from telegram.ext import Updater
 import os
+from os.path import join, dirname
+from longpoling.run import main as run_lognpooling
+from webhook.run import main as run_webhook
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -13,20 +17,38 @@ def hello():
     return "{}:{}:{}:{}".format(port, debug, appname, token)
 
 if __name__ == '__main__':
-    schema = os.environ.get('SCHEMA')
-    if schema not in ('longpooling', 'webhoos'):
-        raise Exception('ALARMA')
+    # Prepare data
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    # Run longpooling mode
+    if os.getenv('SCHEMA') == 'longpooling':
+        data = {
+            'token': os.getenv('TOKEN', 'NO-TOKEN')
+        }
+        run_lognpooling(data)
+    # Run webhook mode
+    elif os.getenv('SCHEMA') == 'webhook':
+        data = {
+            'token': os.getenv('TOKEN'),
+            'appname': os.getenv('APPNAME'),
+            'port': os.getenv('PORT'),
+            'debug': os.getenv('DEBUG')
+        }
+        run_webhook(data)
+    else:
+        raise Exception('ALARMA! SCHEMA NOT VALID!')
 
 
 
-    port = int(os.environ.get('PORT', 5000))
-    debug = bool(os.environ.get('DEBUG', True))
-    appname = os.environ.get('APPNAME', 'NO-APP-NAME')
-    token = os.environ.get('TOKEN', 'NO-TOKEN')
+    # port = int(os.environ.get('PORT', 5000))
+    # debug = bool(os.environ.get('DEBUG', True))
+    # appname = os.environ.get('APPNAME', 'NO-APP-NAME')
+    # token = os.environ.get('TOKEN', 'NO-TOKEN')
 
-    updater = Updater(token)
-    updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
-    updater.bot.set_webhook("https://{}.herokuapp.com/".format(appname) + token)
-    updater.idle()
+    # updater = Updater(token)
+    # updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+    # updater.bot.set_webhook("https://{}.herokuapp.com/".format(appname) + token)
+    # updater.idle()
 
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    # app.run(host='0.0.0.0', port=port, debug=debug)
---
+++ webhook/run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import telegram
+from flask import Flask, request
+from telegram.ext import Updater
+
+app = Flask(__name__)
+
+global bot
+global data_dict
+
+bot = None
+data_dict = None
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
+
+        chat_id = update.message.chat.id
+
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text.encode('utf-8')
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+
+def set_webhook(token, port, appname):
+    updater = Updater(token)
+    # add handlers
+    updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+    updater.bot.set_webhook("https://{}.herokuapp.com/".format(appname) + token)
+    updater.idle()
+
+@app.route('/')
+def index():
+    return '.'
+
+def main(data_dict):
+    token = data_dict['token']
+    appname = data_dict['appmame']
+    port = data_dict['port']
+    debug = data_dict['debug']
+
+    set_webhook(token, port, appname)
+    bot = telegram.Bot(token=token)
+    app.run(host='0.0.0.0', port=port, debug=debug)
